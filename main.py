b4b5711d52afc6e5d45a0f9dea664ad9f1ad2424
Ren-451 <renren.g69@gmail.com>
2024-07-04 01:44:13 +0800

    enter task def

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ window = Tk()
 # give title
 window.title("TODO List App")
 
+# interface
+
 # frame widget
 frame_task = Frame(window)
 frame_task.pack()
@@ -22,13 +24,40 @@ listbox_task.config(yscrollcommand=scrollbar_task.set)
 scrollbar_task.config(command=listbox_task.yview)
 
 # button
-enter_button = Button(window, text="Add", width=50, command=entertask)
-enter_button.pack(pady=3)
+entry_button = Button(window, text="Add", width=50, command=entertask)
+entry_button.pack(pady=3)
 
 del_button = Button(window, text="Delete", width=50, command=deletetask)
 del_button.pack(pady=3)
 
 done_button = Button(window, text="Done", width=50, command=donetask)
 done_button.pack(pady=3)
+
+# definitions
+
+
+def entertask():
+
+    # new popup window to create task
+    input_text = ""
+
+    def add():
+
+        input_text = entry_task.get(1.0, "end - 1c")
+        if input_text == "":
+            tkinter.messagebox.showwarning(title="Warning.", message="Please enter text.")
+        else:
+            listbox_task.insert(END, input_text)
+            # close root1 window
+            root1.destroy()
+
+    root1 = Tk()
+    root1.title("Add a task")
+    entry_task = Text(root1, width=40, height=4)
+    entry_task.pack()
+    button_temp = Button(root1, text="Add a task", command=add)
+    button_temp.pack()
+    root1.mainloop()
+
 
 window.mainloop()
