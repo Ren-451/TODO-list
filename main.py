33be9c826e49b5830bb8770ee3ead155cb14d880
Ren-451 <renren.g69@gmail.com>
2024-07-04 00:33:06 +0800

    widget frame start
    initial progress.

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-#
+# package import
+from tkinter import *
+import tkinter.messagebox
+
+window = Tk()
+
+# give title
+window.title("TODO List App")
+
+# frame widget
+frame_task = Frame(window)
+frame_task.pack()
+
+window.mainloop()
