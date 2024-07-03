7f6485ccd6a1d8e0ec8f8d06fd760474df775bba
Ren-451 <renren.g69@gmail.com>
2024-07-04 01:15:47 +0800

    add hold item listbox

--- main.py
+++ main.py
@@ -11,4 +11,8 @@ window.title("TODO List App")
 frame_task = Frame(window)
 frame_task.pack()
 
+# hold item in listbox
+listbox_task = Listbox(frame_task, bg="#096578", fg="#91e1f0", height=15, width=50, font="Source Code Pro")
+listbox_task.pack(side=tkinter.LEFT)
+
 window.mainloop()
