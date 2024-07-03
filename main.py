219cb6a54f47122aeef3372fa8575f9893966e75
Ren-451 <renren.g69@gmail.com>
2024-07-04 01:25:45 +0800

    add buttons

--- main.py
+++ main.py
@@ -21,4 +21,14 @@ scrollbar_task.pack(side=tkinter.RIGHT, fill=tkinter.Y)
 listbox_task.config(yscrollcommand=scrollbar_task.set)
 scrollbar_task.config(command=listbox_task.yview)
 
+# button
+enter_button = Button(window, text="Add", width=50, command=entertask)
+enter_button.pack(pady=3)
+
+del_button = Button(window, text="Delete", width=50, command=deletetask)
+del_button.pack(pady=3)
+
+done_button = Button(window, text="Done", width=50, command=donetask)
+done_button.pack(pady=3)
+
 window.mainloop()
