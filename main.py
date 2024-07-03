471bdbcf27ae3451d26fbdc7eabefcd670d499bf
Ren-451 <renren.g69@gmail.com>
2024-07-04 01:19:25 +0800

    add scrolldown

--- main.py
+++ main.py
@@ -15,4 +15,10 @@ frame_task.pack()
 listbox_task = Listbox(frame_task, bg="#096578", fg="#91e1f0", height=15, width=50, font="Source Code Pro")
 listbox_task.pack(side=tkinter.LEFT)
 
+# scrolldown
+scrollbar_task = Scrollbar(frame_task)
+scrollbar_task.pack(side=tkinter.RIGHT, fill=tkinter.Y)
+listbox_task.config(yscrollcommand=scrollbar_task.set)
+scrollbar_task.config(command=listbox_task.yview)
+
 window.mainloop()
